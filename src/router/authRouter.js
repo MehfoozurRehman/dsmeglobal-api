930e00ec538f38666a05617481ee131b4abbeca5
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-01-29 16:47:47 +0500

    auth done

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -4,88 +4,38 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
-  // Our register logic starts here
   try {
-    // Get user input
     const { username, email, password, isAdmin } = req.body;
-
-    // Validate user input
-    if (!(email && password && username)) {
-      res.status(400).send("All input is required");
-    }
-
-    // check if user already exist
-    // Validate if user exist in our database
     const oldUser = await User.findOne({ email });
-
     if (oldUser) {
-      return res.status(409).send("User Already Exist. Please Login");
+      return res
+        .status(409)
+        .json({ message: "User Already Exist. Please Login" });
     }
-
-    //Encrypt user password
     encryptedPassword = await bcrypt.hash(password, 10);
-
-    // Create user in our database
-    const user = await User.create({
+    const user = await User({
       username,
-      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      email: email.toLowerCase(),
       password: encryptedPassword,
       isAdmin: isAdmin,
-    });
-
-    // Create token
-    const token = jwt.sign(
-      { user_id: user._id, email },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "2h",
-      }
-    );
-    // save user token
-    user.token = token;
-
-    // return new user
+    }).save();
     res.status(201).json(user);
   } catch (err) {
     console.log(err);
   }
-  // Our register logic ends here
 });
 
 router.post("/login", async (req, res) => {
-  // Our login logic starts here
   try {
-    // Get user input
     const { email, password } = req.body;
-
-    // Validate user input
-    if (!(email && password)) {
-      res.status(400).send("All input is required");
-    }
-    // Validate if user exist in our database
     const user = await User.findOne({ email });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-
-      // save user token
-      user.token = token;
-
-      // user
       res.status(200).json(user);
-    }
-    res.status(400).send("Invalid Credentials");
+    } else res.json({ message: "Invalid Credentials" });
   } catch (err) {
     console.log(err);
   }
-  // Our register logic ends here
 });
 
 module.exports = router;
